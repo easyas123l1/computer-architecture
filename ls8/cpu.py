8d69d37c48cb7b366f7a2eba9b94278e4e319b3e
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-06-17 15:23:00 -0500

    complete run and halt

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,22 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0  # program counter
+        self.running = False
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +28,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +69,33 @@ class CPU:
 
         print()
 
+    def LDI(self):
+        pass
+
+    def PRN(self):
+        pass
+
+    def MUL(self):
+        pass
+
+    def HLT(self):
+        self.pc += 1
+        self.running = False
+
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+        while self.running:
+            # Instruction Register
+            IR = self.ram[self.pc]
+            branch_table = {
+                self.LDI: self.LDI,
+                self.PRN: self.PRN,
+                self.MUL: self.MUL,
+                self.HLT: self.HLT
+            }
+            if IR in branch_table:
+                branch_table[IR]()
+            else:
+                print(f'Unknown instruction: {IR}, at address PC: {self.pc}')
+                sys.exit(1)
