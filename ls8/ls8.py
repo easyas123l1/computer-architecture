da13e2d3d1a5a9879b7b3a5d84e6f135a92c4fe5
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-06-18 15:32:01 -0500

    import programs thru terminal

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,22 @@
 """Main."""
 
 import sys
+import re
 from cpu import *
-
+characters = '\r | a | b | c | d | e | f | g | h | i | j | k | l | m | n | o | p | q | r | s | t | v | w | x | y | z | # | . | ! | ? | ,'
 cpu = CPU()
+filename = sys.argv[1]
+program = []
+with open(filename) as f:
+    for line in f:
+        line = line.split('#')
+        line = ','.join(line).lower()
+        line = re.sub(characters, '', line)
+        line = line.replace('\r', '').replace('\n', '')
+        line = re.sub(characters, '', line)
+        if len(line) > 1:
+            if line[0] == '1' or line[0] == '0':
+                program.append(line[:8])
 
-cpu.load()
+cpu.load(program)
 cpu.run()
